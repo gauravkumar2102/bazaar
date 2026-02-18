7ee6331b802e58d96b590d99531cf3d790b57464
Gaurav kumar <anshusingh2122@gmail.com>
2026-02-18 16:09:16 +0530

    Update index.js

--- micro-marketplace/backend/src/index.js
+++ micro-marketplace/backend/src/index.js
@@ -14,7 +14,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:19006'],
+  origin: ['https://adorable-travesseiro-8cd688.netlify.app','http://localhost:3000', 'http://localhost:5173', 'http://localhost:19006'],
   credentials: true
 }));
 app.use(express.json());
